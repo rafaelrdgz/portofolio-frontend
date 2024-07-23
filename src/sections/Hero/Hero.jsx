2030efbec8f07f27ead2a881cf851ef7290fedd1
rafaelrdgz <rafaelrdgz2003@gmail.com>
2024-07-23 19:14:37 -0400

    deleted download resume

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,6 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/cv.pdf";
 import { useTheme } from "../../theme/ThemeContext";
 
 function Hero() {
